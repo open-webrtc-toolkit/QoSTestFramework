d21e80d974e8bb5cc648ef0065847264d80e49eb
Zhang, Yanbin <yanbin.zhang@intel.com>
2018-05-09 10:50:13 +0800

    update encoded file generation scripts, included vp9 codec encoded file

--- mcu-bench_cpp/videoGenerateScripts/run.sh
+++ mcu-bench_cpp/videoGenerateScripts/run.sh
@@ -2,4 +2,6 @@
 
 export LD_LIBRARY_PATH="/usr/local/lib"
 
-./genTestStream "$@"
+./genTestStream -i football_720p_taged_vp8.mkv
+
+#it will generate vp8 encoded file named as football_720p_taged_vp8.vp8 
